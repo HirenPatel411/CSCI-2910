9ed45b546fb29b8bd28b0089cb2745f302ecbb43
Hiren Patel <64229831+HirenPatel411@users.noreply.github.com>
2022-09-20 14:38:01 -0400

    Add files via upload

---
+++ Lab8-Get-Started-With-ASP_Net-Core-MVC/Hiren_Patel_lab8/Hiren_Patel_lab8/Data/Hiren_Patel_lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace Hiren_Patel_lab8.Data
+{
+    public class Hiren_Patel_lab8Context : DbContext
+    {
+        public Hiren_Patel_lab8Context (DbContextOptions<Hiren_Patel_lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Lab8-Get-Started-With-ASP_Net-Core-MVC/Hiren_Patel_lab8/Hiren_Patel_lab8/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hiren_Patel_lab8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
