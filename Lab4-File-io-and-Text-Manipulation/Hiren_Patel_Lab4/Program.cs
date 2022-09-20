17d1826e9338266eb740a3e1ba8f407abdf0f3a0
Hiren Patel <64229831+HirenPatel411@users.noreply.github.com>
2022-09-20 14:32:00 -0400

    Add files via upload

---
+++ Lab4-File-io-and-Text-Manipulation/Hiren_Patel_Lab4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:  Lab 4
+//	File Name:         Program.cs
+//	Course:            CSCI 2910	
+//	Created:           Thursday, June 3, 2021
+//  Author:            Hiren Patel, patelhm@etsu.edu
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Hiren_Patel_Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This will take a text input or a file path 
+             */
+            Console.WriteLine("Enter text or file path to process.");
+            string userInput = Console.ReadLine();
+            bool isFile = File.Exists(userInput);
+
+            TextManipulator textManipulator = new TextManipulator(userInput, isFile);
+
+            /*
+             * This tests all of the actions required for lab
+             */
+            Console.WriteLine("\nRandom sentence: " + textManipulator.RandomSentence);
+            Console.WriteLine("\nRandom word: " + textManipulator.GetRandomWord());
+            Console.WriteLine("\nNumber of words: " + textManipulator.NumberOfWords());
+            Console.WriteLine("\nCharacter count: " + textManipulator.GetCharacterCount());
+
+            Console.WriteLine("Enter some characters to remove (spaces will be removed as well)");
+            Console.WriteLine(textManipulator.RemoveCharacters(Console.ReadLine().ToCharArray()));
+            
+        }
+    }
+    
+}
---
+++ Lab4-File-io-and-Text-Manipulation/Hiren_Patel_Lab4/TextManipulator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Solution/Project:  Lab 4
+//	File Name:         TextManipulator.cs
+//	Course:            CSCI 2910	
+//	Created:           Thursday, June 3, 2021
+//  Author:            Hiren Patel, patelhm@etsu.edu
+//  Note:              All methods have specefic names and are named after exaclty what they are used for
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+namespace Hiren_Patel_Lab4
+{
+    public class TextManipulator : IDisposable
+    {
+        public List<string> Sentences { get; set; }
+        Random rand = new Random();
+
+        public string FilePath { get; set; }
+        public string Text { get; set; }
+        public string RandomSentence { get; set; }
+
+        public TextManipulator(string text, bool isTextFilePath)
+        {
+            if (isTextFilePath)
+            {
+                this.FilePath = text;
+                if (File.Exists(FilePath))
+                {
+                    this.Text = File.ReadAllText(FilePath);
+                }
+            }
+            else
+            {
+                this.Text = text;
+            }
+
+            Sentences = GetSentencesFromText(Text);
+            if (Sentences.Count < 2)
+            {
+                throw new Exception("Error. You need to have more than one sentence.");
+            }
+
+            SetRandomSentence();
+        }
+
+        private List<string> GetSentencesFromText(string text)
+        {
+            List<string> sentences = new List<string>();
+            string currentSentence = "";
+            foreach (var letter in text)
+            {
+                currentSentence += letter;
+                if (IsEndOfSentence(letter))
+                {
+                    sentences.Add(currentSentence);
+                    currentSentence = "";
+                }
+            }
+            return sentences;
+        }
+
+        public bool IsEndOfSentence(char character)
+        {
+            return character == '!' || character == '?' || character == '.';
+        }
+
+        public void SetRandomSentence()
+        {
+            int index = rand.Next(Sentences.Count);
+            RandomSentence = Sentences.ElementAt(index);
+        }
+
+        public string GetRandomWord()
+        {
+            var words = RandomSentence.Split(' ');
+            int index = rand.Next(words.Length);
+
+            string randomWord = words.ElementAt(index);
+            return randomWord;
+        }
+
+        public int NumberOfWords()
+        {
+            var words = RandomSentence.Split(' ');
+            return words.Length;
+        }
+
+        public string RemoveCharacters(char[] characters)
+        {
+            string newSentence = "";
+            foreach (var letter in RandomSentence)
+            {
+                if (!characters.Contains(letter))
+                {
+                    newSentence += letter;
+                }
+            }
+            return newSentence;
+        }
+
+        public Dictionary<char, int> GetCharacterCount()
+        {
+            Dictionary<char, int> charCount= new Dictionary<char, int>();
+
+            foreach (var letter in RandomSentence)
+            {
+                if (charCount.ContainsKey(letter))
+                {
+                    charCount[letter] += 1;
+                }
+                else
+                {
+                    charCount.Add(letter, 1);
+                }
+            }
+            return charCount;
+        }
+
+        public void Dispose()
+        {
+            Sentences.Clear();
+        }
+    }
+}
